edd62ccd0865ab11d38d53dd8b49c3e3b8339fa4
Afolabi Opakunle <opakunleafolabi@gmail.com>
2020-11-22 17:38:02 +0100

    ready for upload

---
+++ src/components/WelcomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Afolabi</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "WelcomePage",
+  props: {
+    
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import LoginSignUp from '../views/LoginSignUp.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/loginsignup',
+    name: 'LoginSignUp',
+    component: LoginSignUp
   },
   {
     path: '/about',
